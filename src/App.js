432a53df60cf4065406d5bb561f8de70cf6d8cef
Miika Juhala <77334107+miikajuhala@users.noreply.github.com>
2021-12-15 16:36:27 +0200

    Snackbar

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
 
 import './App.css';
+import React from 'react';
 import {Routes, Route} from "react-router-dom";
 import Calendar from './Components/Calendar';
 import Raport from './Components/Raport';
 import Userlist from './Components/Userlist';
 import Menu from './Components/Menu';
 import Traininglist from './Components/Traininglist';
-
+import { Button, IconButton, Snackbar } from '@material-ui/core';
 function App() {
-
-  //yhteiset funktiot 
-  //kehityskohteet:
-  //JOS ON ILMAN NIMEÄ NIIN KAATUU KALENTERI
-  //traininglistiin suora muokkaus?
-  //kalenteriin suora muokkaus tai popupin avaus
-  //csv fle näyimpi formatointi
+  
+  const [open, setOpen] = React.useState(false);
+  const [msg, setMsg] = React.useState("");
 
   
+
+  const handleClose = (event, reason) => {
+    setOpen(false);
+  };
+
+  
+  
   return (
-<div className="App">
+  <div className="App">
   {/* Top menu bar storing links and routes for navigating */}
   <Menu />
 
   {/* Components assigned to router paths for rendering */}
   <Routes>
-        <Route path="/calendar"  element={<Calendar />}></Route>
-        <Route path="/"  element={<Userlist />}></Route>
+        <Route path="/calendar"  element={<Calendar setOpen={setOpen} setMsg={setMsg} />}></Route>
+        <Route path="/"  element={<Userlist setOpen={setOpen} setMsg={setMsg} />}></Route>
         <Route path="/raport"     element={<Raport   />}></Route>
-        <Route path="/userlist"    element={<Userlist />}></Route>
-        <Route path="/traininglist" element={<Traininglist />}></Route>
+        <Route path="/userlist"    element={<Userlist setOpen={setOpen} setMsg={setMsg} />}></Route>
+        <Route path="/traininglist" element={<Traininglist setOpen={setOpen} setMsg={setMsg}/>}></Route>
   </Routes>
+  
 
+<Snackbar
+  open={open}
+  autoHideDuration={6000}
+  onClose={handleClose}
+  message={msg}
+/>
   </div>
   );
 }
